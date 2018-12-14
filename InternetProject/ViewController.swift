97acb084eebeadbfd1b4bfcc2293dfa51730c7f3
HunterKnott <hunterknott00@gmail.com>
2018-12-14 12:06:22 -0700

    Initial Commit

---
+++ InternetProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InternetProject
+//
+//  Created by Knott, Hunter on 12/12/18.
+//  Copyright © 2018 Knott, Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
